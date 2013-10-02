ffe64909d27bed7239894325558862ac75feefcf
Guy Freeman <startup@gfrm.in>
2013-10-03 01:19:15 +0800

    readd 1.R

--- 1.R
+++ 1.R
@@ -9,8 +9,8 @@ pop <- melt(pop)
 colnames(pop)[3] <- "year"
 pop <- data.table(pop)
 pop[sex == "F", value := -value]
-pop[, value := -(value/1000.0)]
 pop$age <- factor(pop$age, levels = c("0 - 4", "5 - 9", "10 - 14", "15 - 19", "20 - 24", "25 - 29", "30 - 34", "35 - 39", "40 - 44", "45 - 49", "50 - 54", "55 - 59", "60 - 64", "65 - 69", "70 - 74", "75 - 79", "80 - 84", "≧ 85"))
+# poptotalage <- pop[,sum(value),by=age]
 theme_bw()
-popplot <- ggplot() + geom_bar(data = pop, mapping = aes(x = age, y = value, group = sex, fill = sex), stat = "identity", position = "identity") + coord_flip() + labs(x = "Age", y = "Population (thousands)") + scale_y_continuous(labels = comma, limits = c(-400, 400)) + facet_wrap(~ year, ncol=2) + theme_bw() + theme(legend.justification=c(1,0), legend.position=c(0.9,0.05))
-ggsave(filename = "popplot.png", plot = popplot, width = 100, height = 250, units = "mm")
+popplot <- ggplot() + geom_area(data = pop, mapping = aes(x = age, y = value, group = sex, fill = sex), stat = "identity", position = "identity") + coord_flip() + labs(x = "Age", y = "Population", title = "Hong Kong's population distribution, 1981-2012") + scale_y_continuous(labels = comma, limits = c(-400000, 400000)) + facet_wrap(~ year) + theme_bw()
+ggsave(filename = "popplot.png", plot = popplot)
